e89c78c70546b6ee54893348e5e9428fe2597378
enrico-bonelli <enrico.bonelli.job@gmail.com>
2016-03-12 10:58:08 +0100

    Test modifiche su github

--- test.js
+++ test.js
@@ -3,5 +3,6 @@
 const http = require('http');
 const porta = 3000;
 var server = http.createServer((req, res)=>{
-
-});
+  res.writeHead(200,{'Content-Type':'text/plain'});
+  res.end('Test commit su github');
+}).listen(porta,()=>console.log(`Server in ascolto su porta ${porta}`));
